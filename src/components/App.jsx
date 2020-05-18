de182817dc847caace50293737302ecdd0fac685
Michael <63890254+kllmmc23@users.noreply.github.com>
2020-05-18 16:24:22 -0600

    extra practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
+var userIsRegistered = true;
 
-function App() {
+export default function App() {
   return (
     <div className="container">
-      <Form />
+      <Form isRegistered={userIsRegistered} />
+
     </div>
   );
 }
 
-export default App;
+
